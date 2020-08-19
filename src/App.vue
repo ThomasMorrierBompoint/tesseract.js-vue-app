8e8f81443ebb6b7b942812c4ddfd54ba103896ce
thomas morrier <thomas@fixrs.ca>
2020-08-19 14:22:57 -0400

    Update

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,8 @@
         <option value="/images/facture-table.png">facture-table.png</option>
         <option value="/images/facture-videotron.png">facture-videotron.png</option>
         <option value="/images/text-russe.jpg">text-russe.jpg</option>
+        <option value="/images/text-manuscript.jpg">text-manuscript.jpg</option>
+        <option value="/images/text-caligraphie.jpg">text-caligraphie.jpg</option>
       </select>
       <select @change="updateLocale" name="locale">
         <option value="eng">Anglais</option>
@@ -17,9 +19,10 @@
       </select>
     </div>
     <img v-if="image" id="text-img" :src="image">
+    <k-progress :percent="Math.floor(progress * 100)" />
     <template v-if="text">
       <div class="result">
-        <h3>Result</h3>
+        <h3>Result: {{ this.image.split('/').slice(-1)[0] }}</h3>
         <div>{{ text }}</div>
       </div>
     </template>
@@ -29,37 +32,53 @@
 <script>
 /* eslint-disable */
 import { createWorker, PSM, OEM } from 'tesseract.js';
-const worker = createWorker({
-  logger: m => console.log(m),
-});
 
 export default {
   name: 'app',
   data() {
     return {
-      text:'',
+      worker: {},
       locale:'eng',
       image: location.href + '/images/testocr.png',
+      text:'',
+      results: {},
+      progress: 0,
     };
+  },
+  mounted() {
+    let self = this;
+    this.worker = createWorker({
+      logger: m => {
+        console.log(m);
+        if (m.status === 'recognizing text') {
+          self.progress = m.progress;
+        }
+      },
+    });
   },
   methods: {
     update(event) {
       this.image = location.href + event.target.value;
+
+      const key  = this.image;
+      this.text = this.results[key];
     },
     updateLocale(event) {
       this.locale = event.target.value;
     },
     async recognize() {
       const img = document.getElementById('text-img');
-      console.log(img);
-      await worker.load();
-      await worker.loadLanguage(this.locale);
-      await worker.initialize(this.locale, OEM.LSTM_ONLY);
-      await worker.setParameters({
+
+      await this.worker.load();
+      await this.worker.loadLanguage(this.locale);
+      await this.worker.initialize(this.locale, OEM.LSTM_ONLY);
+      await this.worker.setParameters({
         tessedit_pageseg_mode: PSM.SINGLE_BLOCK,
       });
-      const { data: { text } } = await worker.recognize(img);
+
+      const { data: { text } } = await this.worker.recognize(img);
       this.text = text;
+      this.results[this.image] = text;
     }
   }
 }
@@ -85,13 +104,12 @@ export default {
 }
 #app img {
   margin: 50px auto auto;
-  max-height: 90%;
-  max-width: 90%;
+  max-height: 60vh;
   border: solid 2px black;
 }
 .result {
   margin: auto;
-  max-height: 50%;
-  max-width: 50%;
+  max-height: 50vh;
+  max-width: 75%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import KProgress from 'k-progress';
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.component('k-progress', KProgress);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
